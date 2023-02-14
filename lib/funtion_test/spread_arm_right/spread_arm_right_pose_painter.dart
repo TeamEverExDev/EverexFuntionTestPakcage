9d365d91a74127ed7167dcc155367c751fd15f44
kim dong joo <50819258+redhotsixbull@users.noreply.github.com>
2023-02-14 17:16:29 +0900

    Update spread_arm_right_pose_painter.dart

--- lib/funtion_test/spread_arm_right/spread_arm_right_pose_painter.dart
+++ lib/funtion_test/spread_arm_right/spread_arm_right_pose_painter.dart
@@ -15,14 +15,62 @@ class SpreadArmRightPosePainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 10.0
       ..color = Color.fromRGBO(7, 190, 184, 1);
+    final bodyPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 10.0
+      ..color = Color.fromRGBO(7, 190, 184, 1);
+
     if (poseModelVo != null) {
+      void paintLine(PosePoint type1, PosePoint type2, Paint paintType) {
+        canvas.drawLine(
+            Offset(translateX(type1.x, width), translateY(type1.y, height)),
+            Offset(translateX(type2.x, width), translateY(type2.y, height)),
+            paintType);
+      }
+
       List<PosePoint?> posePoint = poseModelVo!.getPosePointList();
 
+      ///Draw Circle
       for (PosePoint? k in posePoint) {
         if (k != null) {
           canvas.drawCircle(posePointToOffset(k, width, height), 1, paint);
         }
       }
+
+      //Draw Line
+      paintLine(poseModelVo!.head!, poseModelVo!.neck!, bodyPaint);
+      paintLine(poseModelVo!.neck!, poseModelVo!.centerOfShoulder!, bodyPaint);
+      paintLine(poseModelVo!.centerOfShoulder!, poseModelVo!.leftShoulder!,
+          bodyPaint);
+      paintLine(poseModelVo!.centerOfShoulder!, poseModelVo!.rightShoulder!,
+          bodyPaint);
+      paintLine(
+          poseModelVo!.centerOfShoulder!, poseModelVo!.centerSpine!, bodyPaint);
+      paintLine(
+          poseModelVo!.centerSpine!, poseModelVo!.centerPelvis!, bodyPaint);
+
+      paintLine(
+          poseModelVo!.centerPelvis!, poseModelVo!.leftPelvis!, bodyPaint);
+      paintLine(
+          poseModelVo!.centerPelvis!, poseModelVo!.rightPelvis!, bodyPaint);
+      paintLine(poseModelVo!.rightPelvis!, poseModelVo!.rightKnee!, bodyPaint);
+      paintLine(poseModelVo!.leftPelvis!, poseModelVo!.leftKnee!, bodyPaint);
+      paintLine(poseModelVo!.rightKnee!, poseModelVo!.rightAnkle!, bodyPaint);
+      paintLine(poseModelVo!.rightAnkle!, poseModelVo!.rightTiptoe!, bodyPaint);
+      paintLine(poseModelVo!.rightTiptoe!, poseModelVo!.rightHeel!, bodyPaint);
+
+      paintLine(poseModelVo!.leftKnee!, poseModelVo!.leftAnkle!, bodyPaint);
+      paintLine(poseModelVo!.leftAnkle!, poseModelVo!.leftTiptoe!, bodyPaint);
+      paintLine(poseModelVo!.leftTiptoe!, poseModelVo!.leftHeel!, bodyPaint);
+
+      paintLine(
+          poseModelVo!.rightShoulder!, poseModelVo!.rightElbow!, bodyPaint);
+      paintLine(poseModelVo!.rightElbow!, poseModelVo!.rightWrist!, bodyPaint);
+      paintLine(poseModelVo!.rightWrist!, poseModelVo!.rightPalm!, bodyPaint);
+
+      paintLine(poseModelVo!.leftShoulder!, poseModelVo!.leftElbow!, bodyPaint);
+      paintLine(poseModelVo!.leftElbow!, poseModelVo!.leftWrist!, bodyPaint);
+      paintLine(poseModelVo!.leftWrist!, poseModelVo!.leftPalm!, bodyPaint);
     }
   }
 
