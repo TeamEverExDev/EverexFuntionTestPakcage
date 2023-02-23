2942e154d26d5056cdd29c34cee07adc0580f36b
kim dong joo <50819258+redhotsixbull@users.noreply.github.com>
2023-02-23 15:47:49 +0900

    Update ready_timer_widget.dart

--- lib/funtion_test/util/ready_timer_widget.dart
+++ lib/funtion_test/util/ready_timer_widget.dart
@@ -16,118 +16,236 @@ class ReadyTimerWidget extends StatefulWidget {
 class _ReadyTimerWidgetState extends State<ReadyTimerWidget> {
   @override
   Widget build(BuildContext context) {
-    switch (widget.second) {
-      case 0:
-        return Stack(
-          children: const [
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                "이미지 자리",
-                style: TextStyle(fontSize: 40, color: Colors.red),
-              ),
-            ),
-            Align(
-              alignment: Alignment(
-                0,
-                1,
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(28.0),
+    Orientation viewOrientation = MediaQuery.of(context).orientation;
+
+    if (viewOrientation == Orientation.landscape) {
+      switch (widget.second) {
+        case 0:
+          return Stack(
+            children: const [
+              Align(
+                alignment: Alignment.center,
                 child: Text(
-                  "화면 안에 정면으로 서주세요",
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
+                  "이미지 자리",
+                  style: TextStyle(fontSize: 40, color: Colors.red),
+                ),
+              ),
+              Align(
+                alignment: Alignment(
+                  0,
+                  1,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(28.0),
+                  child: Text(
+                    "화면 안에 정면으로 서주세요",
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        );
-      case 1:
-        return Stack(
-          children: const [
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                "이미지 자리",
-                style: TextStyle(fontSize: 40, color: Colors.red),
-              ),
-            ),
-            Align(
-              alignment: Alignment(
-                0,
-                1,
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(28.0),
+            ],
+          );
+        case 1:
+          return Stack(
+            children: const [
+              Align(
+                alignment: Alignment.center,
                 child: Text(
-                  "3초뒤 시작해주세요",
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
-                  ),
+                  "이미지 자리",
+                  style: TextStyle(fontSize: 40, color: Colors.red),
                 ),
               ),
-            )
-          ],
-        );
-      case 2:
-        return const Align(
-          alignment: Alignment(
-            0,
-            0,
-          ),
-          child: Padding(
-            padding: EdgeInsets.all(28.0),
+              Align(
+                alignment: Alignment(
+                  0,
+                  1,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(28.0),
+                  child: Text(
+                    "3초뒤 시작해주세요",
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          );
+        case 2:
+          return const Align(
+            alignment: Alignment(
+              0,
+              0,
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(28.0),
+              child: Text(
+                "3",
+                style: TextStyle(
+                  fontSize: 108,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          );
+        case 3:
+          return const Align(
+            alignment: Alignment(
+              0,
+              0,
+            ),
             child: Text(
-              "3",
+              "2",
               style: TextStyle(
                 fontSize: 108,
                 color: Colors.white,
                 fontWeight: FontWeight.w700,
               ),
             ),
-          ),
-        );
-      case 3:
-        return const Align(
-          alignment: Alignment(
-            0,
-            0,
-          ),
-          child: Text(
-            "2",
-            style: TextStyle(
-              fontSize: 108,
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
+          );
+        case 4:
+          return const Align(
+            alignment: Alignment(
+              0,
+              0,
             ),
-          ),
-        );
-      case 4:
-        return const Align(
-          alignment: Alignment(
-            0,
-            0,
-          ),
-          child: Padding(
-            padding: EdgeInsets.all(28.0),
+            child: Padding(
+              padding: EdgeInsets.all(28.0),
+              child: Text(
+                "1",
+                style: TextStyle(
+                  fontSize: 108,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          );
+        default:
+          return Container();
+      }
+    } else {
+      switch (widget.second) {
+        case 0:
+          return Stack(
+            children: const [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "이미지 자리",
+                  style: TextStyle(fontSize: 40, color: Colors.red),
+                ),
+              ),
+              Align(
+                alignment: Alignment(
+                  0,
+                  1,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(28.0),
+                  child: Text(
+                    "화면 안에\n정면으로 서주세요",
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        case 1:
+          return Stack(
+            children: const [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "이미지 자리",
+                  style: TextStyle(fontSize: 40, color: Colors.red),
+                ),
+              ),
+              Align(
+                alignment: Alignment(
+                  0,
+                  1,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(28.0),
+                  child: Text(
+                    "3초 뒤\n시작해주세요",
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          );
+        case 2:
+          return const Align(
+            alignment: Alignment(
+              0,
+              0,
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(28.0),
+              child: Text(
+                "3",
+                style: TextStyle(
+                  fontSize: 108,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          );
+        case 3:
+          return const Align(
+            alignment: Alignment(
+              0,
+              0,
+            ),
             child: Text(
-              "1",
+              "2",
               style: TextStyle(
                 fontSize: 108,
                 color: Colors.white,
                 fontWeight: FontWeight.w700,
               ),
             ),
-          ),
-        );
-      default:
-        return Container();
+          );
+        case 4:
+          return const Align(
+            alignment: Alignment(
+              0,
+              0,
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(28.0),
+              child: Text(
+                "1",
+                style: TextStyle(
+                  fontSize: 108,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          );
+        default:
+          return Container();
+      }
     }
   }
 }
